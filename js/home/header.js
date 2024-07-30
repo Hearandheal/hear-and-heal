f795035fd88e75ae3a4335162d86459f01438b5b
divyashantkumarCS <divyashant@checkedspot.com>
2024-07-30 12:16:14 +0530

    updateD

---
+++ js/home/header.js
@@ -0,0 +1,9 @@
+const heroImage = document.getElementById('heroImageContainer');
+
+
+function heroimageTrasition() {
+    heroImage.style.top = "50%";
+    heroImage.style.opacity = "100%";
+}
+
+heroimageTrasition();
